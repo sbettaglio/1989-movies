eb649173edd65ec8432d1886058d3359f39e27dc
Stefan Bettaglio <sbettaglio@gmail.com>
2020-03-21 13:44:08 -0400

    can display returned movies data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 import React, { Component } from 'react'
 import HelloWorld from './components/HelloWorld'
 
-const App = () => {
-  return <HelloWorld />
+class App extends React.Component {
+  state = {
+    movies: [],
+  }
+
+  componentDidMount() {
+    // once the app is added to the page, make an API call to load the data
+    fetch(
+      'https://api.themoviedb.org/3/discover/movie?primary_release_year=1989&sort_by=popularity.desc&api_key=e02fcd07e30880bdc4771c0d0564e80e'
+    )
+      .then(response => {
+        console.log(response)
+        return response.json()
+      })
+      .then(data => {
+        console.log(data)
+        this.setState({
+          movies: data.results,
+        })
+      })
+  }
+
+  render() {
+    return (
+      <>
+        <h1>1989 Movies</h1>
+        <ul>
+          {this.state.movies.map(movie => {
+            return (
+              <li key={movie.id}>
+                <h2>{movie.title}</h2>
+                <p>
+                  <span>Overview:</span> {movie.overview}
+                </p>
+              </li>
+            )
+          })}
+        </ul>
+      </>
+    )
+  }
 }
 
 export default App
