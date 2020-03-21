8896bd1de76b0bd51ba00f430fc965aa9583c6cf
Stefan Bettaglio <sbettaglio@gmail.com>
2020-03-21 15:04:06 -0400

    explorer mode

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,12 +5,14 @@ class Movies extends Component {
     return (
       <li key={this.props.id}>
         <h2>{this.props.title}</h2>
-        <img
-          src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${this.props.poster_path}`}
-        />
-        <p>
-          <span>Overview:</span> {this.props.overview}
-        </p>
+        <section>
+          <img
+            src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${this.props.poster_path}`}
+          />
+          <p>
+            <span>Overview:</span> {this.props.overview}
+          </p>
+        </section>
       </li>
     )
   }
